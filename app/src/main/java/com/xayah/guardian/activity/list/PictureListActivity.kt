108ca1ef20142909e2e3adac157f8f46b84625e6
Xayah <zds1249475336@gmail.com>
2022-07-27 21:47:43 +0800

    feat: 上传按钮改至人脸数据卡片
    Change-Id: Idadd1de826bb4e9a59e749dbc8595e84ef59e76e

--- app/src/main/java/com/xayah/guardian/activity/list/PictureListActivity.kt
+++ app/src/main/java/com/xayah/guardian/activity/list/PictureListActivity.kt
@@ -1,38 +1,30 @@
 package com.xayah.guardian.activity.list
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.ViewModelProvider
 import com.drakeet.multitype.MultiTypeAdapter
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.gson.Gson
 import com.xayah.guardian.App
 import com.xayah.guardian.adapter.PictureListAdapter
 import com.xayah.guardian.data.Picture
 import com.xayah.guardian.databinding.ActivityPictureListBinding
-import com.xayah.guardian.util.*
-import com.xayah.guardian.view.setWithEdit
-import com.xayah.materialyoufileexplorer.MaterialYouFileExplorer
+import com.xayah.guardian.util.Server
+import com.xayah.guardian.util.fastInitialize
+import com.xayah.guardian.util.readDeviceInfo
+import com.xayah.guardian.util.savePicturesNum
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.File
 
 class PictureListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPictureListBinding
     private lateinit var viewModel: PictureListViewModel
-    private lateinit var materialYouFileExplorer: MaterialYouFileExplorer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
-        materialYouFileExplorer = MaterialYouFileExplorer().apply {
-            initialize(this@PictureListActivity)
-        }
 
         binding = ActivityPictureListBinding.inflate(layoutInflater)
         viewModel = ViewModelProvider(this)[PictureListViewModel::class.java]
@@ -42,7 +34,7 @@ class PictureListActivity : AppCompatActivity() {
         initialize()
     }
 
-    fun initialize() {
+    private fun initialize() {
         binding.toolbar.setNavigationOnClickListener { finish() }
 
         val deviceInfo = App.globalContext.readDeviceInfo()
@@ -70,42 +62,6 @@ class PictureListActivity : AppCompatActivity() {
                 items = adapterList
             }
             fastInitialize()
-        }
-
-        binding.floatingActionButton.setOnClickListener {
-            materialYouFileExplorer.apply {
-                isFile = true
-                suffixFilter = ArrayList("jpg,jpeg,png".split(","))
-                filterWhitelist = true
-                defPath = "/storage/emulated/0/DCIM"
-
-                toExplorer(it.context) { path, _ ->
-                    val context = this@PictureListActivity
-                    MaterialAlertDialogBuilder(context).apply {
-                        setWithEdit(context, GlobalString.editName, "") {
-                            BottomSheetDialog(context).apply {
-                                setLoading()
-                                CoroutineScope(Dispatchers.IO).launch {
-                                    Server.picturesUpload(it, deviceInfo.device_code, File(path)) {
-                                        dismiss()
-                                        if (it.code != 1) {
-                                            Toast.makeText(
-                                                this@PictureListActivity,
-                                                GlobalString.uploadFailed,
-                                                Toast.LENGTH_SHORT
-                                            ).show()
-                                        } else {
-                                            CoroutineScope(Dispatchers.Main).launch {
-                                                initialize()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
         }
     }
 }
--- app/src/main/java/com/xayah/guardian/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/xayah/guardian/fragment/home/HomeFragment.kt
@@ -7,10 +7,13 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.ObservableBoolean
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dylanc.activityresult.launcher.StartActivityLauncher
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.gson.Gson
 import com.king.zxing.CameraScan
 import com.king.zxing.CaptureActivity
@@ -18,18 +21,19 @@ import com.xayah.guardian.App
 import com.xayah.guardian.data.Authorize
 import com.xayah.guardian.data.DeviceInfo
 import com.xayah.guardian.databinding.FragmentHomeBinding
-import com.xayah.guardian.util.Server
-import com.xayah.guardian.util.readRTMPAddress
-import com.xayah.guardian.util.readRTMPCarAddress
-import com.xayah.guardian.util.saveDeviceInfo
+import com.xayah.guardian.util.*
+import com.xayah.guardian.view.setWithEdit
+import com.xayah.materialyoufileexplorer.MaterialYouFileExplorer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: HomeViewModel
+    private lateinit var materialYouFileExplorer: MaterialYouFileExplorer
     private lateinit var startActivityLauncher: StartActivityLauncher
 
     override fun onCreateView(
@@ -176,6 +180,46 @@ class HomeFragment : Fragment() {
                 }
             }
         }
+        binding.materialButtonRecord.setOnClickListener {
+            val view = it
+            materialYouFileExplorer.apply {
+                isFile = true
+                suffixFilter = ArrayList("jpg,jpeg,png".split(","))
+                filterWhitelist = true
+                defPath = "/storage/emulated/0/DCIM"
+
+                toExplorer(it.context) { path, _ ->
+                    val context = requireContext()
+                    MaterialAlertDialogBuilder(context).apply {
+                        setWithEdit(context, GlobalString.editName, "") {
+                            BottomSheetDialog(context).apply {
+                                setLoading()
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    Server.picturesUpload(
+                                        it,
+                                        viewModel.deviceInfo.device_code,
+                                        File(path)
+                                    ) {
+                                        dismiss()
+                                        if (it.code != 1) {
+                                            Toast.makeText(
+                                                context,
+                                                GlobalString.uploadFailed,
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        } else {
+                                            CoroutineScope(Dispatchers.Main).launch {
+                                                viewModel.toPictureListActivity(view)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         binding.materialButtonRefresh.setOnClickListener {
             initialize()
         }
@@ -186,6 +230,9 @@ class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startActivityLauncher = StartActivityLauncher(requireActivity())
+        materialYouFileExplorer = MaterialYouFileExplorer().apply {
+            initialize(requireActivity())
+        }
     }
 
     override fun onResume() {
